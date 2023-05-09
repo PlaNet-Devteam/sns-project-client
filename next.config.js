20cb97bdf1c73f28a90b70e87fbf931ed5acae36
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-05-09 18:23:05 +0900

    feat: svg파일 config 설정 및 scss import 설정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-const path = require("path");
+/** @type {import('next').NextConfig} */
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
