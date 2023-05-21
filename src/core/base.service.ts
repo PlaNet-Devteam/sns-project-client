203d13ddd0cb16cc564588653b26f98c89f33207
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-05-21 12:59:57 +0900

    refactor: 쿠키서비스 수정

--- src/core/base.service.ts
+++ src/core/base.service.ts
@@ -1,5 +1,7 @@
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import axios from 'axios';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/core/utils/jwt-storage';
 import JwtStorageService from '@/core/utils/jwt-storage';
+import { RESPONSE_STATUS } from './enum';
 
 export const api = axios.create({
   baseURL: 'http://localhost:4300',
@@ -9,17 +11,56 @@ export const api = axios.create({
   withCredentials: true,
 });
 
+const token = JwtStorageService.getToken(ACCESS_TOKEN);
+
 api.interceptors.request.use(
-  (config: InternalAxiosRequestConfig<any>) => {
-    if (typeof window !== 'undefined') {
-      const accessToken = JwtStorageService.getToken();
-      if (accessToken) {
-        config.headers['Authorization'] = `Bearer ${accessToken}`;
-      }
+  (config) => {
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
-    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const { status, data } = error.response;
+    console.log(' error.response', error.response);
+    // 토큰이 없을 때
+    if (data.error === RESPONSE_STATUS.NO_ACCESS_TOKEN) {
+      location.replace('/login');
+    }
+
+    // 엑세스 토큰 만료
+    if (data.error === RESPONSE_STATUS.ACCESS_TOKEN_EXP) {
+      const { data } = await axios.post(
+        'http://localhost:4300/auth/refresh-token',
+        null,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      console.log('dddafdfadf', data);
+      // JwtStorageService.removeToken(ACCESS_TOKEN);
+      return;
+    }
+
+    // 리프레시 토큰이 없거나 만료되었을 때
+    if (
+      data.error === RESPONSE_STATUS.NO_REFRESH_TOKEN ||
+      data.error === RESPONSE_STATUS.REFRESH_TOKEN_EXP
+    ) {
+      JwtStorageService.removeToken(ACCESS_TOKEN);
+      JwtStorageService.removeToken(REFRESH_TOKEN);
+      return;
+    }
   },
 );
