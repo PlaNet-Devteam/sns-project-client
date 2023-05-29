35107c89022de6fd1b74b02c12a625db0f3318e3
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-05-30 05:37:30 +0900

    refactor: 레이아웃 좌우 여백 @mixin 추가

--- src/pages/signup/complete/index.tsx
+++ src/pages/signup/complete/index.tsx
@@ -5,26 +5,28 @@ import ButtonGroup from '@/components/common/ButtonGroup';
 
 const SinUpComplete = () => (
   <div className="signup-complete grid">
-    <div className="container content-area">
+    <div className="content-area">
       <div className="middle-area">
-        {/** TODO : 컴포넌트  */}
-        <div className="title-group text-center">
-          <div className="title-group__icon">
-            <IconCircleCheck />
+        <div className="inner-container">
+          {/** TODO : 컴포넌트  */}
+          <div className="title-group text-center">
+            <div className="title-group__icon">
+              <IconCircleCheck />
+            </div>
+            <div className="title-group__text">
+              <h3 className="title-group__title">회원가입이 완료되었습니다</h3>
+              <p className="title-group__dscr">
+                지금 바로 플래닛 SNS 서비스를
+                <br />
+                이용해보세요!
+              </p>
+            </div>
+            <ButtonGroup>
+              <Button to="/login" size="md" variant="primary" isEnglish isFull>
+                Login
+              </Button>
+            </ButtonGroup>
           </div>
-          <div className="title-group__text">
-            <h3 className="title-group__title">회원가입이 완료되었습니다</h3>
-            <p className="title-group__dscr">
-              지금 바로 플래닛 SNS 서비스를
-              <br />
-              이용해보세요!
-            </p>
-          </div>
-          <ButtonGroup>
-            <Button to="/login" size="md" variant="primary" isEnglish isFull>
-              Login
-            </Button>
-          </ButtonGroup>
         </div>
       </div>
     </div>
