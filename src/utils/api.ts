be34e27e9b94dbc6e1b7549706a65b3628fa11c0
jeongminsang <zjzj@hanmail.net>
2023-06-02 16:20:17 +0900

    refactor: util 코드 리팩토링

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,14 @@
 import axios from 'axios';
 
-export const getFeeds = () => axios.get('https://example.com/feeds');
+const baseURL = process.env.NEXT_PUBLIC_FEED_API;
+
+export const baseInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getFeeds = () => {
+  return baseInstance.get('/feeds');
+};
