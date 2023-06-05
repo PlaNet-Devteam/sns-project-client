70ae6b9c18f06fa5979e03f6e352fa01a49defa3
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-06 05:08:43 +0900

    feat: bottomNav 및 topHeader 컴포넌트 추가

---
+++ src/components/nav/bottomNav/BottomNav.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import IconNavFeed from '@/assets/icons/icon_nav_feed.svg';
+import IconNavExplore from '@/assets/icons/icon_nav_explore.svg';
+import IconNavAdd from '@/assets/icons/icon_nav_add.svg';
+import IconNavDM from '@/assets/icons/icon_nav_dm.svg';
+import IconNavProfile from '@/assets/icons/icon_nav_profile.svg';
+import BottomNavItem from './BottomNavItem';
+
+const bottmNavRoutes = [
+  {
+    path: '/feed',
+    name: '메인피드',
+    icon: <IconNavFeed />,
+  },
+  {
+    path: '/explore',
+    name: '피드검색',
+    icon: <IconNavExplore />,
+  },
+  {
+    path: '/feed/create',
+    name: '피드생성',
+    icon: <IconNavAdd />,
+  },
+  {
+    path: '/message',
+    name: '메세지',
+    icon: <IconNavDM />,
+  },
+  {
+    path: '/profile',
+    name: '프로필',
+    icon: <IconNavProfile />,
+  },
+];
+
+const BottomNav = () => {
+  return (
+    <div className="bottom-nav__container">
+      <div className="bottom-nav">
+        {bottmNavRoutes.map((nav) => (
+          <BottomNavItem
+            path={nav.path}
+            name={nav.name}
+            icon={nav.icon}
+            key={nav.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BottomNav;
