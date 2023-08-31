22e55a2eb314430a9437565b4e4a70c15164c77a
jeongminsang <zjzj@hanmail.net>
2023-08-31 15:44:55 +0900

    feat: dayjs 유틸함수 분리

---
+++ src/utils/formattedDate.ts
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
+export const formattedDate = () => {
+  const setConvertTime = (time: string | undefined) => {
+    return dayjs(time).add(9, 'h').fromNow();
+  };
+
+  const dayDiff = (time: string | undefined) => {
+    if (dayjs().diff(time) > 8) {
+      return setConvertTime(time);
+    }
+    return dayjs(time).format('YYYY년 M월 D일');
+  };
+  return dayDiff;
+};
