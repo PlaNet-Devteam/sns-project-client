07fd80678e19ca3777b0944bc82f8a0238d81b11
jeongminsang <zjzj@hanmail.net>
2023-10-04 21:26:48 +0900

    feat: message 기능 추가

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import { FaPaperPlane } from 'react-icons/fa';
+import { AiFillPlusSquare } from 'react-icons/ai';
+import { useRecoilValue } from 'recoil';
+import { userState } from '@/store/userAtom';
+
+const socket = io('http://localhost:8080/');
+console.log(socket);
 
 const Message = () => {
-  return <div>Message</div>;
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState<any>([]);
+
+  const user = useRecoilValue(userState);
+
+  useEffect(() => {
+    socket.on('message', (data: string) => {
+      setMessageList([...messageList, data]);
+    });
+  }, [messageList]);
+
+  const handleSend = () => {
+    socket.emit('message', message);
+    console.log(message);
+    setMessage('');
+  };
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('socket server connected.');
+    });
+    socket.on('disconnect', () => {
+      console.log('socket server disconnected.');
+    });
+  }, []);
+
+  return (
+    <div className="message__container">
+      <h1>{user?.username}</h1>
+      <div className="message__contents">
+        <ul>
+          {messageList.map((msg: string, index: number) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="msg">
+        <div className="message__form">
+          <button>
+            <AiFillPlusSquare color="white" />
+          </button>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="메시지 보내기"
+          />
+          <button onClick={handleSend}>
+            <FaPaperPlane className="message__submit" color="white" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
