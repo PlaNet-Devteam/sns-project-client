04c372c683b213ef7af9ff170d9dd2a4fbdf16e6
jeongminsang <zjzj@hanmail.net>
2023-10-05 22:02:50 +0900

    feat: 채팅 보낸사람 구분 기능 추가

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react';
 import { FaPaperPlane } from 'react-icons/fa';
 import { AiFillPlusSquare } from 'react-icons/ai';
 import { useRecoilValue } from 'recoil';
+import classNames from 'classnames';
 import { userState } from '@/store/userAtom';
+import MsgHeader from '@/components/message/MsgHeader';
 
 const socket = io('http://localhost:8080/');
 console.log(socket);
@@ -17,12 +19,13 @@ const Message = () => {
 
   useEffect(() => {
     socket.on('message', (data: string) => {
+      console.log(data);
       setMessageList([...messageList, data]);
     });
   }, [messageList]);
 
   const handleSend = () => {
-    socket.emit('message', message);
+    socket.emit('message', [user?.username, message]);
     console.log(message);
     setMessage('');
   };
@@ -37,28 +40,39 @@ const Message = () => {
   }, []);
 
   return (
-    <div className="message__container">
-      <h1>{user?.username}</h1>
-      <div className="message__contents">
-        <ul>
+    <div className="chat__container">
+      <MsgHeader />
+      <div className="message__container">
+        <ul className="message__contents">
           {messageList.map((msg: string, index: number) => (
-            <li key={index}>{msg}</li>
+            <li
+              className={classNames({
+                'message__my-content': msg[0] === user?.username,
+                'message__other-content': msg[0] !== user?.username,
+              })}
+              key={index}
+            >
+              {msg[1]}
+            </li>
           ))}
         </ul>
-      </div>
-      <div className="msg">
         <div className="message__form">
           <button>
-            <AiFillPlusSquare color="white" />
+            <AiFillPlusSquare className="message__option" color="#1ba2f3" />
           </button>
           <input
             type="text"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             placeholder="메시지 보내기"
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                handleSend();
+              }
+            }}
           />
           <button onClick={handleSend}>
-            <FaPaperPlane className="message__submit" color="white" />
+            <FaPaperPlane className="message__submit" color="#1ba2f3" />
           </button>
         </div>
       </div>
