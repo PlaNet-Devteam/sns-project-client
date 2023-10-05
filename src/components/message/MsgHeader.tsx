5a0304650770ae91493d1a5770fdf7e106ecaa98
jeongminsang <zjzj@hanmail.net>
2023-10-05 22:03:38 +0900

    feat: chat header 추가

---
+++ src/components/message/MsgHeader.tsx
@@ -0,0 +1,12 @@
+const MsgHeader = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="header__button">뒤로</div>
+        <div className="header__content">채팅하기</div>
+        <div className="header__button">설정</div>
+      </div>
+    </>
+  );
+};
+export default MsgHeader;
