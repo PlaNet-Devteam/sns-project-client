7185ecd35af6042cee9f6ef255017577f4675704
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-10-18 19:28:44 +0900

    refactor: dependency 관련 주석 eslint 주석 추가

--- src/hooks/useIsScrolling.ts
+++ src/hooks/useIsScrolling.ts
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useState } from 'react';
 export function useIsScrolling() {
   const [isScrolling, setIsScrolling] = useState(false);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleScroll = () => {
     if (!isScrolling) {
       setIsScrolling(true);
