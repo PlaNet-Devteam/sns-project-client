5b29e46aafdde443e90a3c98dbf40b5fe1e2a9fb
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-11-16 22:45:12 +0900

    refactor: ProfileFeedListItem  이미지 복수 아이콘 표시

--- src/components/profile/ProfileFeedListItem.tsx
+++ src/components/profile/ProfileFeedListItem.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import { useSetRecoilState } from 'recoil';
+import { IoMdAlbums } from 'react-icons/io';
 import {
   feedImageState,
   feedModalState,
   isFeedModalOpenState,
 } from '@/store/feedAtom';
 import { FeedType } from '@/core/types/feed';
+import styles from './ProfileFeedListItem.module.scss';
 
 interface ProfileFeedListItemProps {
   queryKey: string[];
@@ -30,20 +32,20 @@ function ProfileFeedListItem({ queryKey, item }: ProfileFeedListItemProps) {
 
   return (
     <>
-      <div
-        className={'profile-feed-item'}
-        onClick={onClickFeedModalOpenHandler}
-      >
-        <figure className={'profile-feed-item__image'}>
+      <div className={styles.item} onClick={onClickFeedModalOpenHandler}>
+        <figure className={styles.image}>
           {item?.feedImages && item?.feedImages.length > 0 ? (
-            item?.feedImages[0] && (
+            <>
+              {item?.feedImages.length > 1 && (
+                <IoMdAlbums className={styles.icon} />
+              )}
               <Image
                 src={`${process.env.NEXT_PUBLIC_AWS_S3_BUCKET}${item?.feedImages[0].image}`}
                 alt=""
                 width={200}
                 height={200}
               />
-            )
+            </>
           ) : (
             <Image
               src={'/img/icons/icon_default_profile.svg'}
