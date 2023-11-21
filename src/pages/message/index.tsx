f2b10e794eddfec0af0c5b047aef5e54979c71ec
jeongminsang <zjzj@hanmail.net>
2023-11-22 01:38:42 +0900

    feat: 채팅방 리스트 추가

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import io from 'socket.io-client';
+import { useRouter } from 'next/navigation';
 import MsgHeader from '@/components/message/MsgListHeader';
 import { userState } from '@/store/userAtom';
 
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
+
+interface roomListProps {
+  user1: string | undefined;
+  user2: string | undefined;
+  roomId: string;
+}
+
 const MessageList = () => {
+  const router = useRouter();
   const user = useRecoilValue(userState);
+  const [roomList, setRoomList] = useState<roomListProps[]>([]);
+
+  const handleLinkDMpage = (roomId: string) => {
+    router.push(`/message/${roomId}`);
+  };
+
+  useEffect(() => {
+    socket.emit('get_room_list');
+    socket.on('get_room_list', (data: roomListProps[]) => {
+      setRoomList(data);
+    });
+  }, []);
+
   return (
     <div className="msglist__container">
       <MsgHeader username={user?.username} />
-      <div className="none__chatroom">채팅방이 없습니다.</div>
+      <div className="msglist">
+        {roomList ? (
+          roomList.map((list) => (
+            <div
+              className="valid__chatroom"
+              onClick={() => handleLinkDMpage(list.roomId)}
+            >
+              {user?.username === list.user1 ? list.user2 : list.user1}
+            </div>
+          ))
+        ) : (
+          <div className="none__chatroom">채팅방이 없습니다.</div>
+        )}
+      </div>
     </div>
   );
 };
