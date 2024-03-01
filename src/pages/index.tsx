9fe164bdb7999004aec2db0dddc73132933bef6a
BRWEB <52031484+rockbell89@users.noreply.github.com>
2024-03-01 17:07:03 +0900

    feat: meta 태그 http-equiv 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,10 @@ export default function Home() {
         <title>PlaNet</title>
         <meta name="description" content="우주 테마 소셜 네트워크" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <IntroVisual />
